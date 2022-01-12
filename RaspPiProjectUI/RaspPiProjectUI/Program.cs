60cb14837df87d179007f917d6d9ccacb7c2dad2
Kamil Bajorek <elbajorro@gmail.com>
2022-01-12 22:42:06 +0100

    Change mon/Winforms to Eto

--- RaspPiProjectUI/RaspPiProjectUI/Program.cs
+++ RaspPiProjectUI/RaspPiProjectUI/Program.cs
@@ -1,22 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using Eto.Drawing;
+using Eto.Forms;
+using System;
 
 namespace RaspPiProjectUI
 {
-    internal static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            new Application(Eto.Platform.Detect).Run(new MainForm());
         }
     }
 }
